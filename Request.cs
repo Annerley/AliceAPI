055059e7d6f8734d62b948a9d0fd4f7284a606bc
Дарья Лапутина <laputina975@gmail.com>
2021-06-05 23:52:42 +0300

    added new methods

--- Request.cs
+++ Request.cs
@@ -193,5 +193,99 @@ namespace AliceAPI
             return sb.ToString().Trim();
         }
 
+        public string firstLesson(string weekDay, string student)
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand searchGroup = new MySqlCommand("SELECT `group_id`, `id` FROM students WHERE `name` = @name", db.getConnection());
+            searchGroup.Parameters.Add("@name", MySqlDbType.VarChar).Value = student;
+            MySqlDataReader reader = searchGroup.ExecuteReader();
+            reader.Read();
+            string group_id = reader[0].ToString();
+            string student_id = reader[1].ToString();
+            reader.Close();
+
+
+            MySqlCommand searchSubgroup = new MySqlCommand("SELECT `subgroup_id` FROM `group_to_student` WHERE `student_id` = @st_id", db.getConnection());
+            searchSubgroup.Parameters.Add("@st_id", MySqlDbType.VarChar).Value = student_id;
+            reader = searchSubgroup.ExecuteReader();
+            reader.Read();
+            string sub_id = reader[0].ToString();
+            reader.Close();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT MIN(lesson_number) FROM schedule WHERE `group_id` = @group_id AND (`subgroup` = @subgroup OR `subgroup` IS NULL) AND `week_day` = @week", db.getConnection());
+
+            cmd.Parameters.Add("@group_id", MySqlDbType.VarChar).Value = group_id;
+            cmd.Parameters.Add("@subgroup", MySqlDbType.VarChar).Value = sub_id;
+            cmd.Parameters.Add("@week", MySqlDbType.VarChar).Value = weekDay;
+
+            StringBuilder sb = new StringBuilder();
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            sb.Append(reader[0].ToString());
+
+            reader.Close();
+            db.closeConnection();
+
+            return sb.ToString().Trim();
+        }
+
+        public string breakStart(string lessonNumber)
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand searchGroup = new MySqlCommand("SELECT `start_time` FROM break_schedule WHERE `number` = @number", db.getConnection());
+            searchGroup.Parameters.Add("@name", MySqlDbType.Int32).Value = lessonNumber;
+            MySqlDataReader reader = searchGroup.ExecuteReader();
+            reader.Read();
+            return reader[0].ToString().Trim();
+            
+           
+        }
+        public string breakStartAndFinish(string lessonNumber)
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand searchGroup = new MySqlCommand("SELECT `start_time`, `end_time` FROM break_schedule WHERE `number` = @number", db.getConnection());
+            searchGroup.Parameters.Add("@name", MySqlDbType.Int32).Value = lessonNumber;
+            MySqlDataReader reader = searchGroup.ExecuteReader();
+            reader.Read();
+            return reader[0].ToString().Trim()+" "+reader[1].ToString().Trim();
+        }
+
+        public string breakFinish(string lessonNumber)
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand searchGroup = new MySqlCommand("SELECT`end_time` FROM break_schedule WHERE `number` = @number", db.getConnection());
+            searchGroup.Parameters.Add("@name", MySqlDbType.Int32).Value = lessonNumber;
+            MySqlDataReader reader = searchGroup.ExecuteReader();
+            reader.Read();
+            return reader[0].ToString().Trim();
+        }
+
+        public string schedule()
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand searchGroup = new MySqlCommand("SELECT * FROM break_schedule", db.getConnection());
+            
+            MySqlDataReader reader = searchGroup.ExecuteReader();
+            StringBuilder sb = new StringBuilder();
+            while (reader.Read())
+            {
+                sb.Append(reader[0].ToString() + " пара начинается в ");
+                sb.Append(reader[1].ToString() + " и заканчивается в");
+                sb.Append(reader[2].ToString() + " ");
+
+            }
+            return sb.ToString();
+        }
+
+        public string windowScheduleWeek(string weekDay, string student)
+        {
+            //в процессе
+            return "";
+        }
     }
 }
